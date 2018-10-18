ad63056016d37e77f1387af09dc68047ddef0f1f
High School Student <rhs@rlas-macair-39.local>
2018-10-18 12:44:30 -0500

    hackwich

--- OptionaHackwich/ViewController.swift
+++ OptionaHackwich/ViewController.swift
@@ -10,16 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var firstNameText: UITextField!
+    @IBOutlet weak var middleNameText: UITextField!
+    @IBOutlet weak var lastNameText: UITextField!
+    @IBOutlet weak var signUpNameButton: UIButton!
+    @IBOutlet weak var nameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    signUpNameButton.layer.cornerRadius = 5
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+  
+    @IBAction func signUpButton(_ sender: Any) {
+        guard let firstName = firstNameText.text, (!(firstNameText.text?.isEmpty)!)
+            else {
+                alert(text: "First Name" )
+                return
+        }
+        guard let middleName = middleNameText.text, (!(middleNameText.text?.isEmpty)!)
+            else {
+                alert(text: "Middle Name" )
+                return
+        }
+        guard let lastName = lastNameText.text, (!(lastNameText.text?.isEmpty)!)
+            else {
+                alert(text: "Middle Name" )
+                return
+        }
     }
-
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
 }
 
